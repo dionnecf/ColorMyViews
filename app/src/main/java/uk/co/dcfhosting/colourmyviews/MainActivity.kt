a1fb73f8a66a0c8ee89b64d13e8597881671cfea
Dionne Condor-Farrell <info@dcfhosting.co.uk>
2020-12-05 23:35:11 +0000

    Added 3 buttons to change the colour of bot Three, Box Four, and Box Five

--- app/src/main/java/uk/co/dcfhosting/colourmyviews/MainActivity.kt
+++ app/src/main/java/uk/co/dcfhosting/colourmyviews/MainActivity.kt
@@ -20,7 +20,10 @@ class MainActivity : AppCompatActivity() {
                 box_two_text,
                 box_three_text,
                 box_four_text,
-                box_five_text)
+                box_five_text,
+                red_button,
+                yellow_button,
+                green_button)
 
         for(item in clickableViews) {
             item.setOnClickListener {
@@ -31,11 +34,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeBoxColourChange(view: View) {
         when(view.id) {
+
+            //Change boxes using Color class colors for background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Change boxed using Android color resources for background
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            //Change Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
